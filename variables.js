b542105ebbdcf04a5b958a08f76047312c30e7d7
sadia0522 <sadiashimu87@gmail.com>
2026-02-01 11:08:32 +0600

    variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const name = "Shimu";
+let email ='sadiaislam0000@gmail.com' ;
+var password = 12917217364198;
+let username;
+
+console.table({name,email,password,username})
+// const name = "sadia"
+console.table({name,email,password,username})
+var password = 12312312
